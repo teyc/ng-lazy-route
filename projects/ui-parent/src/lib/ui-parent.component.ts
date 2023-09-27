8e3c553e8224c152316eaa0250640e667e862ba3
Chui Tey <extern.chtey@allianz-assistance.com.au>
2023-09-28 09:12:01 +1000

    Service automatically injected

--- projects/ui-parent/src/lib/ui-parent.component.ts
+++ projects/ui-parent/src/lib/ui-parent.component.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
+import { UiChildComponent, UiChildService } from 'projects/ui-child/src/public-api';
 
 @Component({
   selector: 'lib-ui-parent',
   template: `
     <p>
-      ui-parent works!
+      ui-parent works! (Answer is {{ answer }})
     </p>
   `,
   styles: [
-  ]
+  ],
 })
 export class UiParentComponent {
-
+  answer: Number
+  constructor(uiChildService: UiChildService) {
+    this.answer = uiChildService.getAnswer()
+  }
 }
